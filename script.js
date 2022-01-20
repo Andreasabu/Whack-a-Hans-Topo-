22e9cd40d291821c3669483014ad3de64c1e339e
Emely Alonzo <emelyalonzom@gmail.com>
2022-01-20 17:59:33 +0100

    add basic functionality

--- script.js
+++ script.js
@@ -1,48 +1,122 @@
+/* seleccionamos los elementos de HTML */
+const squares = document.querySelectorAll(".square");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+const gameOver = document.querySelector(".gameOver");
 
-const holes = document.querySelectorAll(".hole"); 
-const mole = document.querySelector(".mole");
-const timeLeft = document.querySelector("#timeLeft")
-const scoreBoard = document.getElementsByClassName("score");
 
+//Función que ejecuta el juego
+const startGame = () => {
+
+  //Creamos las variables con los valores iniciales del juego
+  let result = 0; //0 puntos
+  let hitPosition; //Posición donde está el topo
+  let currentTime = 60; //Tiempo máximo de juego en segundos
+  let timerId = null;
+
+  /* Creamos el topo */
+  const mole = document.createElement("div");
+  mole.innerHTML = `
+<img src="./images/mole.png" height="200" width="200">
+`;
+
+  function randomSquare() {
+    let randomSquare = squares[Math.floor(Math.random() * 9)]; // Seleccionamos el proximo agujero donde va a salir el topo
+    randomSquare.appendChild(mole); //Ponemos al topo en ese agujero
+    console.log(`estoy en ${randomSquare.id}`); // Comprobamos en qué cuadrado estamos
+    hitPosition = randomSquare.id; // Guardamos externamente en qué posición está el topo
+    //Si no se ha dado click en el topo
+    squares.forEach((square) => {
+      // Recorremos los cuadrados de nuevo
+      square.removeChild(mole); // y eliminamos el topo (para no tener un topo por cada movimiento)
+    });
+  }
+
+  //Escuchamos si el usuario hace click en algún agujero que para ello realizamos un recorrido por todos los agujeros
+  squares.forEach((square) => {
+    square.addEventListener("click", () => {
+      //Si el usuario ha dado click en el agujero donde está el topo
+      if (square.id == hitPosition) {
+        result++; //Sumamos un punto
+        score.textContent = result;
+        hitPosition = null; //Cambiamos la posición del topo (por eso le damos el valor de null para que no pise)
+        square.removeChild(mole); //Eliminamos al topo de ese agujero
+      }
+    });
+  });
+
+  /* Creamos la función para que el topo se mueva a los segundos que queremos */
+  function moveMole() {
+    timerId = setInterval(randomSquare, 1000); //En este caso cada 1 segundo (1000)
+  }
+  //Movemos los topos por el tablero
+  moveMole();
+
+  /* Realizamos la cuenta atrás del contador del juego*/
+  function countDown() {
+    currentTime--; //Elimina 1 unidad
+    timeLeft.textContent = currentTime;
+    /* Hacemos que cuando el contador llegue a 0 se muestre el cartel de game over */
+    if (currentTime == 0) {
+      clearInterval(countDownTimerId);
+      clearInterval(timerId);
+      /* Añadimos etiquetas para que indique la puntuación final del juego */
+      gameOver.innerHTML = `
+        <h2>GAME OVER!</h2>
+        <h5 class="caja">Puntuación: ${result}</h5>
+        <p class="caja2">Tu invento te ha ganado</p>
+        `;
+    }
+  }
+
+  // Invocamos la función de cuenta atrás haciendo que decrezca segundo a segundo
+  let countDownTimerId = setInterval(countDown, 1000);
+};
+
+//Al hacer click en start, se ejecuta el juego
+document.getElementById("btn-start").addEventListener("click", () => {
+  startGame();
+});
+
+
+/*************************************************************************************************************************/
 /* let lastHole;
 
 let isFinished = false; */
 
 // let score = 0;
-let result = 0
+// let result = 0
 
+// const timeForHansTopo = () => {
+//     //Esta función creará el tiempo randomizado para que los Hans Topo salgan de los hoyos aleatoriamente
+//     //Usando el método Math.random()
+// }
 
-const timeForHansTopo = () => {
-    //Esta función creará el tiempo randomizado para que los Hans Topo salgan de los hoyos aleatoriamente
-    //Usando el método Math.random()
-}
+// const randomHole = () => {
+//     holes.forEach(hole => {
+//         hole.classList.remove("mole") //se remueven los topos
+//     })
+//    let randomPosition = holes[Math.floor(Math.random()*6)] //recorre aleatoriamente y multiplicar el numero de ollos y redondea, porque multiplica.
+//    randomPosition.classList.add("mole")
+// }
+// randomHole();
 
-const randomHole = () => {
-    holes.forEach(hole => {
-        hole.classList.remove("mole") //se remueven los topos
-    })
-   let randomPosition = holes[Math.floor(Math.random()*6)] //recorre aleatoriamente y multiplicar el numero de ollos y redondea, porque multiplica.
-   randomPosition.classList.add("mole")
-}
-randomHole();
+// const timeOutside = () => {
+//Esta función determina el tiempo que los topos deben estar asomados
+// }
 
-const timeOutside = () => {
-    //Esta función determina el tiempo que los topos deben estar asomados
-}
-
-const startGame = () => {
-    scoreBoard.textContent = 0;
-    isFinished = false;
-    score = 0;
-    peep();
-    setTimeout(() => timeUp = true, 15000) 
+// const startGame = () => {
+//     scoreBoard.textContent = 0;
+//     isFinished = false;
+//     score = 0;
+//     peep();
+//     setTimeout(() => timeUp = true, 15000)
 //muestra topos aleatoriamente durante 15 segundos
-}
+// }
 
-const wack = () => {
-    //Esta función sumará un punto al marcador y quitará la clase "up" del elemento clickado para ocultar al topo
-}
-
+// const wack = () => {
+//Esta función sumará un punto al marcador y quitará la clase "up" del elemento clickado para ocultar al topo
+// }
 
 //Con un bucle for each asignamos la función "wack" al evento "click" en cada topo
-moles.forEach(mole => mole.addEventListener('click', wack));
+// moles.forEach(mole => mole.addEventListener('click', wack));
